4078b776ae1f5343a7e3c1afde60f1c5d35b843a
Hongwei <bhw8412@hotmail.com>
2021-04-15 00:15:10 +1000

    del irrelevant privilege

--- src/main/java/com/hongwei/model/privilege/Privilege.kt
+++ src/main/java/com/hongwei/model/privilege/Privilege.kt
@@ -2,7 +2,6 @@ package com.hongwei.model.privilege
 
 data class Privilege(
         val entries: List<String>,
-        val blog: BlogPrivilege? = BlogPrivilege(),
         val photo: PhotoPrivilege? = PhotoPrivilege(),
         val uploadExercise: Boolean? = false
 )
